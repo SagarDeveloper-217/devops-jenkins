31a71d8459fddeb093133b320f5f868936d67b81
Sagar Pilla <sagarluckyguy217@gmail.com>
2024-06-20 14:02:44 +0530

    first commit

---
+++ devops-jenkins/src/main/java/devops_integration/DevopsJenkinsApplication.java
@@ -0,0 +1,13 @@
+package devops_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsJenkinsApplication.class, args);
+	}
+
+}
---
+++ devops-jenkins/src/test/java/devops_integration/DevopsJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package devops_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
